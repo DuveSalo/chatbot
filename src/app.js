b7d21cdbb1d7b70089c7cc56baf6ec9c7a0e0c9e
DuveSalo <duvefutbo@gmail.com>
2025-01-14 10:46:05 -0300

    Descripción de los cambios

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import { createBot } from '@builderbot/bot'
+import { createBot, createProvider } from '@builderbot/bot'
 import { MongoAdapter as Database } from '@builderbot/database-mongo'
-import { provider } from './provider/index.js';
+import { BaileysProvider } from '@builderbot/provider-baileys'
 import { config } from './config/index.js';
 import flows from './flow/index.js';
 
@@ -9,29 +9,13 @@ const PORT = config.PORT
 const main = async () => {
     const { httpServer } = await createBot ({
         flow: flows,
-        provider: provider,
+        provider: createProvider(BaileysProvider),
         database: new Database({
             dbUri: config.mongoDb_uri,
             dbName: config.mongoDb_name,
         })
     })
     
-<<<<<<< HEAD
-=======
-    const adapterProvider = createProvider(Provider)
-        const adapterDB = new Database({
-        dbUri: config.mongoDb_uri,
-        dbName: config.mongoDb_name,
-    })
-
-    const { httpServer } = await createBot({
-        flow: adapterFlow,
-        provider: adapterProvider,
-        database: adapterDB,
-    })
-
-    
->>>>>>> 3e486d3cc9792e1f3c668115ef79c539dbc7d832
     httpServer(+PORT)
 }
     
