b6b22eb1937ef12bfc5c4ab14ea4a1fc3bee604a
DuveSalo <144804931+DuveSalo@users.noreply.github.com>
2025-06-23 10:24:57 -0300

    Update inicio.js

--- src/flow/presupuesto/sistema/inicio.js
+++ src/flow/presupuesto/sistema/inicio.js
@@ -2,8 +2,6 @@ import { addKeyword, EVENTS } from "@builderbot/bot";
 import { flowDeterminarGrupo } from './grupo.js';
 
 export const flowPreguntasIniciales = addKeyword(EVENTS.ACTION)
-.addAction(async () => {
-})
     .addAnswer('Para proporcionarte un presupuesto para un sistema de autoprotección, necesito la siguiente información:')
     .addAnswer('¿Cuál es la actividad principal de la empresa?', { capture: true }, async (ctx, { state }) => {
         await state.update({ actividad: ctx.body });
